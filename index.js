21b5306593f4c391063074a1f613aab3df803eaf
TrevinBarnes <TrevinBarnes@users.noreply.github.com>
2024-02-03 20:09:06 -0600

    finished product

--- index.js
+++ index.js
@@ -6,15 +6,11 @@
 let id = 0;
 
 document.getElementById('add').addEventListener('click', ()=>{
-    let addedFood = new Text ();
-    let table = document.getElementById('list')
-    let row = table.insertRow(1);
-    row.setAttribute('id', `item-${id}`)
-    row.insertCell(0).innerHTML = document.querySelector('#new-inputType').value;
-    row.insertCell(1).innerHTML = document.querySelector('#new-inputGroup').value;
-    let actions = row.insertCell(2);
-
-
+    let table = document.getElementById('Table1')
+    let row = table.insertRow(1);// creates the new row
+    //row.setAttribute('id', `item-${id}`)
+    row.insertCell(0).innerHTML = document.querySelector('#new-inputType').value;// adds the food type to the row
+    row.insertCell(1).innerHTML = document.querySelector('#new-inputGroup').value;//adds the food group to the row
 });
 
 
