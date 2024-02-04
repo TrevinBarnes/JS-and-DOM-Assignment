42ed5ea400b652447a21a4e423aba669741ab1cb
TrevinBarnes <TrevinBarnes@users.noreply.github.com>
2024-02-03 18:05:36 -0600

    start JS

---
+++ index.js
@@ -0,0 +1,16 @@
+//need an array for food types
+//need an array for food groups
+// need a way to push new elements into table
+// need the survey to ask for food type then group
+
+let i = 0;
+
+document.getElementById('add').addEventListener('click', ()=>{
+ let 
+
+});
+
+
+
+
+
