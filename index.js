64bcf19f4143667dd44618671ec66a80ac972d08
TrevinBarnes <TrevinBarnes@users.noreply.github.com>
2024-02-03 19:08:51 -0600

    line 11 in js is not working

--- index.js
+++ index.js
@@ -3,10 +3,17 @@
 // need a way to push new elements into table
 // need the survey to ask for food type then group
 
-let i = 0;
+let id = 0;
 
 document.getElementById('add').addEventListener('click', ()=>{
- let 
+    let addedFood = new Text ();
+    let table = document.getElementById('list')
+    let row = table.insertRow(1);
+    row.setAttribute('id', `item-${id}`)
+    row.insertCell(0).innerHTML = document.querySelector('#new-inputType').value;
+    row.insertCell(1).innerHTML = document.querySelector('#new-inputGroup').value;
+    let actions = row.insertCell(2);
+
 
 });
 
